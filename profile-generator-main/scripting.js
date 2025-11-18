7a856626c5dd97b18f2ae36189a50e5a89531f76
1984secant <127aacrooms@frhsd.com>
2025-11-18 13:58:35 -0500

    updated

--- profile-generator-main/scripting.js
+++ profile-generator-main/scripting.js
@@ -1,1 +1,24 @@
+let namebox = document.getElementById("nameInput");
+let agebox = document.getElementById("ageInput");
+let colorbox = document.getElementById("colorInput");
+let petbox = document.querySelectorAll("input[type='radio']");
+let gamebox = document.querySelectorAll("input[type='checkbox']");
+let subtn = document.getElementById("submitBtn");
+let profile = document.getElementById("output");
+console.log(gamebox);
+console.log(petbox);
 
+subtn.addEventListener('click', yourname);
+
+function yourname() {
+   console.log(namebox.value);
+   console.log(agebox.value);
+}
+
+// let askname = prompt("What is your name?", "Your name here...");
+
+// namebox.innerHTML = askname; 
+// console.log("Hello, " + namebox + "!");
+
+//let t = namebox.innerHTML += "this";
+//console.log(t);
