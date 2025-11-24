1c81549ec95d9fed20d57f74254f2e47f602e3f2
1984secant <127aacrooms@frhsd.com>
2025-11-24 13:49:24 -0500

    l

--- scripting.js
+++ scripting.js
@@ -13,6 +13,8 @@ subtn.addEventListener('click', yourname);
 function yourname() {
    console.log(namebox.value);
    console.log(agebox.value);
-    var picked = colorbox.options[colorbox.selectedIndex].text;
-   document.getElementById("output").innerHTML = "Hello, my name is " + namebox.value + " and I am " + agebox.value + " years old. My favorite color is " + picked + ".";
+   var picked = colorbox.options[colorbox.selectedIndex].text;
+   var favpet = document.getElementById("petInput").text;
+   var favgame = gamebox.options[gamebox.selectedIndex].text;
+   document.getElementById("output").innerHTML = "Hello, my name is " + namebox.value + " and I am " + agebox.value + " years old. My favorite color is " + picked + ", and my favorite pet is a " + favpet + ". I enjoy " + favgame + ".";
 }
